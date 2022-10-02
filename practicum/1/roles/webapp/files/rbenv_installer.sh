6d0df101848445469682f5a3450c53bf262496f0
3ng1n33r <3ng1n33r@protonmail.com>
2022-10-02 16:30:07 +0500

    a ton of changes

--- practicum/1/roles/webapp/files/rbenv_installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "task#1"
-curl -fsSL "https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer" | bash
-echo "task#2"
-echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bash_profile && source ~/.bash_profile
-echo "task#3"
-rbenv install 2.6.10 && rbenv global 2.6.10 && ruby -v
